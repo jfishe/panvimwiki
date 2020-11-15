99522d843b9eb50c139c7436c5977f5720e7761b
John D. Fisher <jdfenw@gmail.com>
2020-11-15 15:09:49 -0800

    tests(test_catdiary_20170427): asset not only once
    Assert concatenated diary contains 'not' only once.
    
    docs(test_catdiary_20170427): describe Parameters and Raises.

--- tests/test_catvimwiki.py
+++ tests/test_catvimwiki.py
@@ -1,9 +1,12 @@
 """Test concatenate Vimwiki diary files."""
 
 import datetime
+import fileinput
+import re
 from pathlib import Path
 
 import pytest
+
 from vimwiki_docx.catvimwiki import catdiary, get_last_monday, get_last_thursday
 
 
@@ -43,20 +46,38 @@ def test_get_last_monday(test_input: str, expected: str):
         "2017-04-27",
     ],
 )
-def test_catdiary(test_input):
-    """TODO: Docstring for test_catdiary.
+def test_catdiary_20170427(test_input):
+    """Concatenate Vimwiki diary entries two non-contiguous days.
+
+    Concatenate `vimwiki/diary/2017-04-24.wiki` and
+    `vimwiki/diary/2017-04-26.wiki`. When provided enddate 2017-04-27
+    Use get_last_monday(test_input) for startdate. Should not error when other
+    days between Monday and Thursday are missing.
 
     Parameters
     ----------
-    function : TODO
+    test_input : ISO format date 2017-04-27.
 
     Returns
     -------
-    TODO
+    None
+
+    Raises
+    ------
+    AssertionError
+        If concatenated diary contains `not`, more than once.
 
     """
     wikidiary: Path = Path(__file__).parents[0] / "vimwiki/diary"
     enddate: datetime.date = datetime.date.fromisoformat(test_input)
     startdate: datetime.date = get_last_monday(enddate)
     diaryout: Path = catdiary(startdate, enddate, wikidiary)
-    assert diaryout.exists
+
+    pattern = re.compile(r"not", re.IGNORECASE)
+    containsnot = []
+    with fileinput.input(diaryout, openhook=fileinput.hook_encoded("utf-8")) as fin:
+        for line in fin:
+            research = re.search(pattern, line)
+            if research is not None:
+                containsnot.append(research.string)
+    assert len(containsnot) == 1, containsnot
