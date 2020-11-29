f80a6805482a922de200eb379b5cb5e16eb43675
John D. Fisher <jdfenw@gmail.com>
2020-11-29 14:09:29 -0800

    build(setup): update classifiers

--- setup.py
+++ setup.py
@@ -20,8 +20,17 @@ setup(
     package_dir={'': 'src'},
 
     install_requires=requirements,
-    keywords='vimwiki_docx',
+    keywords=['vimwiki_docx', 'vimwiki', 'pandoc', 'python', 'conda'],
     classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Text Editors',
+        'Topic :: Text Editors :: Text Processing',
     ]
 )
