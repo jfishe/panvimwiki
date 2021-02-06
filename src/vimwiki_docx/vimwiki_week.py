d130e75597af40f0eab154c5f524bf16b10460a6
John D. Fisher <jdfenw@gmail.com>
2021-02-06 14:29:34 -0800

    feat(vimwiki_week): add concatenate_diary
    feat(concatenate_diary): convert and choose defaults for string dates
    and path to catdiary input.

---
+++ src/vimwiki_docx/vimwiki_week.py
@@ -0,0 +1,52 @@
+"""Convert str dates to datetime and Path. Set default days for missing dates."""
+
+import datetime
+
+from pathlib import Path
+
+from vimwiki_docx.catvimwiki import catdiary, get_last_monday, get_last_thursday
+
+
+def concatenate_diary(
+    diary_path: str, start_date: str = None, end_date: str = None
+) -> Path:
+    """Concatenate Vimwiki diary from start_date through end_date.
+
+    If the start and end dates are None, concatenate Vimwiki Diary from
+    Monday - Thursday. E.g., Thursday through Sunday returns this week.
+    Monday - Wednesday returns last week.
+
+    Parameters
+    ----------
+    diary_path : Vimwiki Diary absolute path
+
+    start_date : ISO date, e.g., '2017-10-10', the same or earlier than
+                 `end_date`. Defaults to the previous Monday before `end_date`,
+                 unless `end_date` is Monday.
+
+    end_date : ISO date, e.g., '2017-10-10', defaults to the previous Thursday
+               before today, unless today is Thursday.
+
+    Returns
+    -------
+    Path to concatenated Vimwiki diary file
+
+    Raises
+    ______
+    ValueError
+        `catdiary()` raises ValueError if `start_date` is after `start_date`
+
+    """
+    if end_date is None:
+        enddate: datetime.date = get_last_thursday()
+    else:
+        enddate = datetime.date.fromisoformat(end_date)
+
+    if start_date is None:
+        startdate: datetime.date = get_last_monday(enddate)
+    else:
+        startdate = datetime.date.fromisoformat(start_date)
+
+    wikidiary: Path = Path(diary_path)
+
+    return catdiary(startdate=startdate, enddate=enddate, wikidiary=wikidiary)
