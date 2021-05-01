712b9c8658a925eb1ec4069ade9877d2a3192e69
John D. Fisher <jdfenw@gmail.com>
2021-05-01 06:44:20 -0700

    build: add package doc string

--- src/vimwiki_docx/__init__.py
+++ src/vimwiki_docx/__init__.py
@@ -1,3 +1,5 @@
+"""Vimwiki Diary tools for conversion to Microsoft Word docx."""
 from ._version import get_versions
-__version__ = get_versions()['version']
+
+__version__ = get_versions()["version"]
 del get_versions
