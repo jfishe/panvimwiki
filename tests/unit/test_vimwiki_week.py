2a051ce759ab72da43aa0b7812b7709cacc7d1fe
John D. Fisher <jdfenw@gmail.com>
2021-05-01 06:51:43 -0700

    tests: add vimwiki_week

---
+++ tests/unit/test_vimwiki_week.py
@@ -0,0 +1,8 @@
+"""Unit Tests convert Vimwiki diary files."""
+
+from vimwiki_docx.vimwiki_week import concatenate_diary
+
+
+def test_convert_last_week():
+    """Test convert_last_week."""
+    assert False
